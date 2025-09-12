f696b7e6df97ff8bd09aea7e17dc71c42408ae6d
Tim Holm <timothy.j.holm@gmail.com>
2025-09-12 11:32:12 +1000

    remove unused variables.

--- aws/cloudfront/module/scripts/preserve-auth.js
+++ aws/cloudfront/module/scripts/preserve-auth.js
@@ -1,8 +1,3 @@
-// Template in the base paths from the manifest
-// A comma separated list of base paths
-var basePaths = "${base_paths}";
-var allBasePaths = basePaths.split(",").sort((a, b) => b.length - a.length);
-
 function handler(event) {
   var request = event.request;
   var uri = request.uri;
